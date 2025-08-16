f7b86244df1da4a0096b80ffbe8b68d966464e93
Nagmani001 <nagmanipd3@gmail.com>
2025-08-16 22:19:20 +0545

    removed priave key

--- backend/dist/config/utils.js
+++ backend/dist/config/utils.js
@@ -4,9 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PLATFORM_KEYPAIR = exports.PLATFORM_PUBLIC_KEY = exports.PLATFORM_FEE = void 0;
+require('dotenv').config();
 const web3_js_1 = require("@solana/web3.js");
 const bs58_1 = __importDefault(require("bs58"));
 exports.PLATFORM_FEE = 3;
-exports.PLATFORM_PUBLIC_KEY = new web3_js_1.PublicKey("FfS6HXPCMU1NsVQciD66x7Q37gUdwZVviEdggZozES7a");
-const decoded = bs58_1.default.decode("BVpN4sMhVnRyNhG7F6soDHFBDQdJxFt3EqNoZz5X2eeeCrzwXNDD6xZH5foKuBDmhPuCWU1GwL7vxWVvkPP2G1v");
+exports.PLATFORM_PUBLIC_KEY = new web3_js_1.PublicKey(process.env.PLATFORM_PUBLIC_KEY || "");
+const decoded = bs58_1.default.decode(process.env.PLATFORM_PRIVATE_KEY || "");
 exports.PLATFORM_KEYPAIR = web3_js_1.Keypair.fromSecretKey(decoded);
--- backend/src/config/utils.ts
+++ backend/src/config/utils.ts
@@ -1,7 +1,8 @@
+require('dotenv').config()
 import { Keypair, PublicKey } from "@solana/web3.js";
 import bs58 from 'bs58';
 
 export const PLATFORM_FEE = 3;
-export const PLATFORM_PUBLIC_KEY = new PublicKey("FfS6HXPCMU1NsVQciD66x7Q37gUdwZVviEdggZozES7a");
-const decoded = bs58.decode("BVpN4sMhVnRyNhG7F6soDHFBDQdJxFt3EqNoZz5X2eeeCrzwXNDD6xZH5foKuBDmhPuCWU1GwL7vxWVvkPP2G1v");
+export const PLATFORM_PUBLIC_KEY = new PublicKey(process.env.PLATFORM_PUBLIC_KEY || "");
+const decoded = bs58.decode(process.env.PLATFORM_PRIVATE_KEY || "");
 export const PLATFORM_KEYPAIR = Keypair.fromSecretKey(decoded);
